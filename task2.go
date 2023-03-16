9f44b94b0e8f8f86155bc249931ec0ac60b13e23
Yurii Tereschenko <yu0303@yandex.ru>
2023-03-16 16:05:59 +0500

    the sorting func has been changed to lambda func

--- task2.go
+++ task2.go
@@ -2,16 +2,19 @@ package main
 
 import "fmt"
 
-func bubbleSortRev(input ...int) []int {
-	for rightShift := 0; rightShift < len(input); rightShift++ {
-		for i := 1; i < len(input)-rightShift; i++ {
-			if input[i] > input[i-1] {
-				input[i], input[i-1] = input[i-1], input[i]
+const size2 = 8
+
+func main() {
+	arr := [size2]int{2, 3, 6, 4, 2, 3, 5, 1}
+	arrRevSorted := func(input [size2]int) [size2]int {
+		for rightShift := 0; rightShift < len(input); rightShift++ {
+			for i := 1; i < len(input)-rightShift; i++ {
+				if input[i] > input[i-1] {
+					input[i], input[i-1] = input[i-1], input[i]
+				}
 			}
 		}
-	}
-	return input
-}
-func main() {
-	fmt.Println(bubbleSortRev(2, 3, 6, 4, 2, 3, 5, 1))
+		return input
+	}(arr)
+	fmt.Println(arrRevSorted)
 }
